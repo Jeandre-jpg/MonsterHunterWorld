8eea47bcba8f30adb50410c2bf0aab0a87ef3105
Jeandré De Villiers <190025@virtualwindow.co.za>
2022-03-23 10:22:48 +0200

    Style changes, display info

---
+++ MonsterHunterInventory/MonsterHunterInventory/Models/Location.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace MonsterHunterInventory.Models
+{
+	public class Location
+
+
+	{
+
+		//public List<Place> Places = new List<Place>();
+
+
+		////constructor is going to fetch our database
+		//public Location()
+		//{
+
+		//	Places = Database.GetAllPlaces();
+		//}
+
+		//public void UpdateCount(string name, int count)
+		//{
+		//	Database.UpdatePlaceCount(name, count);
+
+		//}
+
+		////function to check how many items there are
+
+		//public int GetCount(string name)
+		//{
+		//	foreach (var place in Places) //searching for specific Place
+		//	{
+		//		if (place.Name == name)
+		//		{
+		//			return place.Count;
+		//		}
+
+		//	}
+		//	return -1; //because there are no Items with that name
+		//}
+
+
+	}
+}
+
